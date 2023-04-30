256546bac12986c74833bc4c530e5514963f0f9a
Dominick Confessore <domconfessore@gmail.com>
2023-04-30 15:52:53 -0400

    Created function to return license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,7 +16,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "[MIT License](https://opensource.org/licenses/mit/)";
+  } else if (license === "Apache 2.0") {
+    return "[Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0)";
+  } else if (license === "GNU GPL v3") {
+    return "[GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0.en.html)";
+  } else if (license === "BSD 3-Clause") {
+    return "[BSD 3-Clause](https://opensource.org/licenses/bsd-3-clause/)";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
