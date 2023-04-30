6b126793c6b40458e0dbafcdc9ba03685adb48e8
Dominick Confessore <domconfessore@gmail.com>
2023-04-30 15:41:42 -0400

    Created function to return license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "https://img.shields.io/badge/License-MIT-yellow.svg";
+  } else if (license === "Apache 2.0") {
+    return "https://img.shields.io/badge/License-Apache_2.0-blue.svg"
+  } else if (license === "GNU GPL v3") {
+    return "https://img.shields.io/badge/License-GPLv3-blue.svg"
+  } else if (license === "BSD 3-Clause") {
+    return "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg"
+  } else if (license === "")
+    return ""
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
