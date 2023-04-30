1d6068b99d9f8bfd314de58d370db1be7e0b087e
Dominick Confessore <domconfessore@gmail.com>
2023-04-30 16:07:18 -0400

    Created function to return license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,7 +32,19 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license === "MIT") {
+    return `This project is licensed with [MIT License](https://opensource.org/license/mit/).`;
+  } else if (license === "Apache 2.0") {
+    return `This project is licensed with [Apache License 2.0]https://www.apache.org/licenses/LICENSE-2.0`
+  } else if (license === "GNU GPL v3") {
+    return `This project is licensed with [GNU GPL v3]https://www.gnu.org/licenses/gpl-3.0.en.html`
+  } else if (license === "BSD 3-Clause") {
+    return `This project is licensed with [BSD 3-Clause License]https://opensource.org/license/bsd-3-clause/`
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
